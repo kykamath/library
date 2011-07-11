30aa1bb7f69db7b173e625b581207e1f6741017d
Krishna Kamath <krishna.kamath@gmail.com>
2011-07-10 22:20:33 -0500

    converted to installabe project

---
+++ setup.py
@@ -0,0 +1,15 @@
+'''
+Created on Jul 10, 2011
+
+@author: kykamath
+'''
+from distutils.core import setup
+setup(name='my_library',
+      version='1.0',
+      description='A collection of modules used by different projects.',
+      author='Krishna Y. Kamath',
+      author_email='krishna.kamath@gmail.com',
+      url='https://github.com/kykamath/library',
+      packages=['library']
+      )
+
