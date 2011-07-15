7b2f4042844532041875ef21b56f3eef4c196b3b
Krishna Kamath <krishna.kamath@gmail.com>
2011-07-15 11:18:38 -0500

    minor chanes

--- library/mr_algorithms/demo/using_options.py
+++ library/mr_algorithms/demo/using_options.py
@@ -1,25 +1,13 @@
 '''
-Created on Jul 14, 2011
+Created on Jul 15, 2011
 
 @author: kykamath
 '''
-from mr_algorithms.mrjobwrapper import MRJobWrapper
-import pprint
+import sys
+sys.path.append('../../')
+from mrjobwrapper import MRJobWrapper
+from mr_algorithms.demo.using_options_mr import UsingOptionsMRJob
 
-class WordCountUsingOptions(MRJobWrapper):
-    def configure_options(self):
-        super(WordCountUsingOptions, self).configure_options()
-        self.add_passthrough_option( '--val1', dest='val1', default=':ilab:', help='provide a val1')
-    def load_options(self, args):
-        """Parse stop_words option."""
-        super(WordCountUsingOptions, self).load_options(args)
-        self.val1 = self.options.val1
-    def mapper(self, key, value):
-        for word in value.split():
-            yield word+self.val1, 1
-        self.val1='krishna'
-    def reducer(self, key, values): 
-        yield key, sum(values)
-    
-if __name__ == '__main__':
-    WordCountUsingOptions.run()
+class UsingOptions(MRJobWrapper):
+    def __init__(self, args='-r hadoop'.split()):
+        self.mrjob = UsingOptionsMRJob(args=args)
--- library/tests/mr_algorithms/demo/using_options_tests.py
+++ library/tests/mr_algorithms/demo/using_options_tests.py
@@ -4,22 +4,21 @@ Created on Jul 14, 2011
 @author: kykamath
 '''
 import sys
+from mr_algorithms.demo.using_options import UsingOptions
 sys.path.append('../../../')
-import unittest, os
-from mr_algorithms.demo.using_options import WordCountUsingOptions
-from StringIO import StringIO
+import unittest
 
 testString='Sachin Tendulkar is one century away from reaching 100 international'
 log1 = '../../../data/log1'
 
 class MRJobWrapperTests(unittest.TestCase):
     def setUp(self):
-        self.wordCount = WordCountUsingOptions(args='-r inline'.split())
-    def test_mapper(self): self.assertEqual([(w+self.wordCount.options.val1,1)for w in testString.split()], list(self.wordCount.mapper('', testString)))
-    def test_reducer(self): self.assertEqual([('foo', 2)], list(self.wordCount.reducer('foo', [1, 1])))
+        self.usingOptions = UsingOptions(args='-r inline'.split())
+    def test_mapper(self): self.assertEqual([(w+':ilab:',1)for w in testString.split()], list(self.usingOptions.mapper('', testString)))
+    def test_reducer(self): self.assertEqual([('foo', 2)], list(self.usingOptions.reducer('foo', [1, 1])))
     def test_runJob(self): 
-        for object in [self.wordCount, WordCountUsingOptions()]:
-            self.assertEqual([(w+self.wordCount.val1,1)for w in sorted(testString.split())], list(object.runJob(inputFileList=[log1])))
+        for object in [self.usingOptions, UsingOptions('-r inline'.split())]:
+            self.assertEqual([(w+':ilab:',1)for w in sorted(testString.split())], list(object.runJob(inputFileList=[log1])))
 
 if __name__ == '__main__':
     unittest.main()
