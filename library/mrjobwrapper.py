c371df2b97534a0baaa8e25a9a24db2cd8e634d4
Krishna Kamath <krishna.kamath@gmail.com>
2011-07-15 10:39:24 -0500

    minor changes

--- library/mrjobwrapper.py
+++ library/mrjobwrapper.py
@@ -5,6 +5,7 @@ Created on Jul 14, 2011
 '''
 import sys
 from mrjob.job import MRJob
+from mrjob.conf import dump_mrjob_conf
 
 class WritableObject:
     def __init__(self):
@@ -18,16 +19,12 @@ class MRJobWrapper(MRJob):
     will be using regularly.
     '''
     def __init__(self, *args, **kwargs): super(MRJobWrapper, self).__init__(*args, **kwargs)
-    def _setOptions(self):
-        self.options.python_archives = ['/'.join(__file__.split('/')[:-2])+'/dist/my_library-1.0.tar.gz']
     def runJob(self, inputFileList):
-        self._setOptions()
         self.args = inputFileList
         with self.make_runner() as runner:
             runner.run()
             for line in runner.stream_output(): yield self.parse_output_line(line)
     def runMapper(self, inputFileList):
-        self._setOptions()
         self.args = inputFileList
         reader = self.protocols()[self.DEFAULT_OUTPUT_PROTOCOL or self.DEFAULT_PROTOCOL]
         mapperOutput = WritableObject()
@@ -35,3 +32,10 @@ class MRJobWrapper(MRJob):
         self.run_mapper()
         sys.stdout = sys.__stdout__ 
         for i in filter (lambda a: a != '\n', mapperOutput.content): yield reader.read(i)
+        
+def  updateMRJobConf():
+    conf = {'runners':{
+                'hadoop':{'python_archives': ['/home/kykamath/projects/library/dist/my_library-1.0.tar.gz']}
+                }
+            }
+    with open('/Users/kykamath/.mrjob', 'w') as f: dump_mrjob_conf(conf, f)
