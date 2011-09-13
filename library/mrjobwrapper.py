de4ce4580eb1773c45d9ab244c92bd0dea42fc2f
Krishna Kamath <krishna.kamath@gmail.com>
2011-09-13 10:28:09 -0500

    removed old class from mr job wrapper

--- library/mrjobwrapper.py
+++ library/mrjobwrapper.py
@@ -17,28 +17,6 @@ class WritableObject:
     def write(self, string):
         self.content.append(string)
 
-class MRJobWrapper():
-    ''' MARKED FOR REMOVAL. DO NOT USE THIS.
-    '''
-    def _setOptions(self, **kwargs):
-        self.mrjob.args = kwargs.get('inputFileList', self.mrjob.args)
-        self.mrjob.options.jobconf = combine_dicts(self.mrjob.options.jobconf, kwargs.get('jobconf', self.mrjob.options.jobconf))
-    def runJob(self, **kwargs):
-        self._setOptions(**kwargs)
-        with self.mrjob.make_runner() as runner:
-            runner.run()
-            for line in runner.stream_output(): yield self.mrjob.parse_output_line(line)
-    def runMapper(self, **kwargs):
-        self._setOptions(**kwargs)
-        reader = self.mrjob.protocols()[self.mrjob.DEFAULT_OUTPUT_PROTOCOL or self.mrjob.DEFAULT_PROTOCOL]
-        mapperOutput = WritableObject()
-        self.mrjob.stdout = mapperOutput
-        self.mrjob.run_mapper()
-        sys.stdout = sys.__stdout__ 
-        for i in filter (lambda a: a != '\n', mapperOutput.content): yield reader.read(i)
-    def mapper(self, key, value): return self.mrjob.mapper(key, value)
-    def reducer(self, key, values): return self.mrjob.reducer(key, values)
-        
 def  updateMRJobConf():
     conf = {'runners':{
                 'hadoop':{'python_archives': ['/home/kykamath/projects/library/dist/my_library-1.0.tar.gz']}
