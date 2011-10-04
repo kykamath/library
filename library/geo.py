8f5bacd28c445d69f25af6ee61b7d884c6d53b81
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-04 15:18:55 -0500

    modified geo

--- library/geo.py
+++ library/geo.py
@@ -10,4 +10,5 @@ def parseData(line):
     if len(data)==7: return {'_id':id, 'u': int(data[0]), 'tw': int(data[1]), 'l': [float(data[2]), float(data[3])], 't': datetime.datetime.strptime(data[4], '%Y-%m-%d %H:%M:%S'), 'x': data[5], 'lid': data[6]}
 
 def getLidFromLocation(location): return '%0.3f %0.3f'%(location[0], location[1])
+def getLocationFromLid(lid): return [float(l) for l in lid.split()]
     
--- library/tests/geo_tests.py
+++ library/tests/geo_tests.py
@@ -6,13 +6,11 @@ Created on Oct 4, 2011
 import sys
 sys.path.append('../')
 import unittest
-from geo import getLidFromLocation
+from geo import getLidFromLocation, getLocationFromLid
 
 class GeoTests(unittest.TestCase):
-    def test_getLidFromLocation(self):
-        l1, l2 = [38.929854, -77.027976], [38.899699, -77.089777]
-        self.assertEqual('38.930 -77.028', getLidFromLocation(l1))
-        self.assertEqual('38.900 -77.090', getLidFromLocation(l2))
+    def test_getLidFromLocation(self): self.assertEqual('38.930 -77.028', getLidFromLocation([38.929854, -77.027976]))
+    def test_getLocationFromLid(self): self.assertEqual([38.93, -77.028000000000006], getLocationFromLid('38.930 -77.028'))
 
 if __name__ == '__main__':
     unittest.main()
