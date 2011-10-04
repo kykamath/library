a5196d87d9db931a2170f908a4759222399c8f07
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-04 15:45:24 -0500

    modified geo

--- library/geo.py
+++ library/geo.py
@@ -4,6 +4,10 @@ Created on Oct 4, 2011
 @author: kykamath
 '''
 import datetime
+
+earthRadiusMiles = 3958.761
+earthRadiusKMs = 6371.009
+
 def parseData(line):
     data = line.strip().split('\t')
     if len(data)!=7: data.append(None) 
@@ -11,4 +15,7 @@ def parseData(line):
 
 def getLidFromLocation(location): return '%0.3f %0.3f'%(location[0], location[1])
 def getLocationFromLid(lid): return [float(l) for l in lid.split()]
-    
+def convertMilesToRadians(miles): return miles/earthRadiusMiles
+def convertRadiansToMiles(radians): return radians*earthRadiusMiles
+def convertKMsToRadians(kms): return kms/earthRadiusKMs
+def convertRadiansToKMs(radians): return radians*earthRadiusKMs
--- library/tests/geo_tests.py
+++ library/tests/geo_tests.py
@@ -6,11 +6,16 @@ Created on Oct 4, 2011
 import sys
 sys.path.append('../')
 import unittest
-from geo import getLidFromLocation, getLocationFromLid
+from geo import getLidFromLocation, getLocationFromLid, convertMilesToRadians,\
+    convertRadiansToMiles, convertKMsToRadians, convertRadiansToKMs
 
 class GeoTests(unittest.TestCase):
     def test_getLidFromLocation(self): self.assertEqual('38.930 -77.028', getLidFromLocation([38.929854, -77.027976]))
     def test_getLocationFromLid(self): self.assertEqual([38.93, -77.028000000000006], getLocationFromLid('38.930 -77.028'))
-
+    def test_convertMilesToRadians(self): self.assertEqual(0.017429695806339407, convertMilesToRadians(69))
+    def test_convertRadiansToMiles(self): self.assertEqual(69, convertRadiansToMiles(0.017429695806339407))
+    def test_convertKMsToRadians(self): self.assertEqual(1.5696100884490981e-05, convertKMsToRadians(0.1))
+    def test_convertRadiansToKMs(self): self.assertEqual(6.3710089999999999, convertRadiansToKMs(0.001))
+    
 if __name__ == '__main__':
     unittest.main()
