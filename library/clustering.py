38a9b03d13f82ff405a0429ee61918025c20df71
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-15 00:21:08 -0500

    modified clustering

--- library/clustering.py
+++ library/clustering.py
@@ -117,6 +117,7 @@ class MultistepItemsetClustering:
             print len(commonItems), float(smallerClusterLength), len(commonItems)/float(smallerClusterLength)
         assert len(commonItems)/float(smallerClusterLength)<=1.0
         if commonItems: print len(commonItems), float(smallerClusterLength), len(commonItems)/float(smallerClusterLength), self.mergeThreshold
+        else: print len(commonItems), float(smallerClusterLength), len(commonItems)/float(smallerClusterLength), self.mergeThreshold
         if len(commonItems)/float(smallerClusterLength)>=self.mergeThreshold: return True
         return False
     def cluster(self, itemsetIterator, itemDistanceFunction, mergeThreshold):
