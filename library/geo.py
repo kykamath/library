0a3ec64269219ce2636cce38b051379ad36017c7
Krishna Kamath <krishna.kamath@gmail.com>
2011-11-20 13:25:20 -0600

    modified mrjob

--- library/geo.py
+++ library/geo.py
@@ -131,6 +131,11 @@ def getRadiusOfGyration(points):
     centerOfMass = getCenterOfMass(points)
     return math.sqrt((sum(getHaversineDistance(centerOfMass, point)**2 for point in points))/len(points))
 
+def getLattice(point, accuracy=0.0075):
+    ''' Accuracy in miles getHaversineDistance([0, 0], [0, 0.0075])
+    '''
+    return [int(point[0]/accuracy)*accuracy, int(point[1]/accuracy)*accuracy]
+
 def getLatticeLid(point, accuracy=0.0075):
     ''' Accuracy in miles getHaversineDistance([0, 0], [0, 0.0075])
     '''
@@ -139,8 +144,8 @@ def getLatticeLid(point, accuracy=0.0075):
 #getLatticeBoundingBoxFor([[0,-10], [10,0]], [2,2], [2.5, -7.5])
 #print convertRadiansToMiles(49-24)
 #print getLatticeLid([37.065,-122.640381])
-#print getLatticeLid([37.073,-122.640381])
-#print getHaversineDistance([0, 0], [0.1, 0])
+print getLattice([37.073,-122.640381], 1.45)
+#print getHaversineDistance([0, 0], [01.45, 0])
 #print getHaversineDistance([-115.29750900000001, 36.181283000000001], [-115.29750900000001, 36.186214])
 #print getCenterOfMass([[-115.303551,36.181283],[-115.297509,36.181283],[-115.297509,36.186214],[-115.303551,36.186214]])
 #print getHaversineDistance([38.533449900000001, -121.4920635], [38.533449900000001, -121.4920635])
