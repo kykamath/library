5196cbf04facde0385e005aa6df7c125f96334c4
Krishna Kamath <krishna.kamath@gmail.com>
2011-12-06 15:33:39 -0600

    modified status

--- library/stats.py
+++ library/stats.py
@@ -3,6 +3,8 @@ Created on Oct 22, 2011
 
 @author: kykamath
 '''
+from scipy import stats
+
 def getWeitzmanOVL(mu1, mu2, sd1, sd2):
     ''' Code by user whuber of http://stats.stackexchange.com/
     http://stats.stackexchange.com/questions/12209/percentage-of-overlapping-regions-of-two-normal-distributionn
@@ -20,3 +22,12 @@ def getWeitzmanOVL(mu1, mu2, sd1, sd2):
     if mu1==1 and mu2==1 and sd1==0 and sd2==0: return (0.0, 0.0)
     value = str(r.integrate(robjects.r['min.f1f2'], float('-inf'), float('inf'), mu1=mu1, mu2=mu2, sd1=sd1, sd2=sd2)).split()
     return (float(value[0]), float(value[-1]))
+
+def getOutliersRangeUsingIRQ(data):
+    '''
+    Uses outlier detection using  Interquartile Ranges suggested in http://www.purplemath.com/modules/boxwhisk3.htm
+    '''
+    q1 = stats.scoreatpercentile(data, 25)
+    q3 = stats.scoreatpercentile(data, 75)
+    iqr = q3-q1
+    return [q1-1.5*iqr, q3+1.5*iqr]
