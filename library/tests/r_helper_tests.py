886ddac78fe150f0727a12c7d077ebff0d3c38d2
Krishna Kamath <krishna.kamath@gmail.com>
2012-09-14 10:32:02 -0500

    modified the module for R methods

--- library/tests/r_helper_tests.py
+++ library/tests/r_helper_tests.py
@@ -20,13 +20,23 @@ class R_HelperTests(unittest.TestCase):
         prediction_variable = 'Life.Exp'
         predictor_variables = ['Population', 'Income', 'Illiteracy',
                                'Murder', 'HS.Grad', 'Frost', 'Area']
-        expected_variables = ['Population', 'Murder', 'HS.Grad', 'Frost']
         self.assertEqual(
-                    expected_variables, 
+                    ['Population', 'Murder', 'HS.Grad', 'Frost'], 
                      R_Helper.variable_selection_using_backward_elimination(
                                                        data_frame,
                                                        prediction_variable,
                                                        predictor_variables
+                                                    )
+                     )
+        predictor_variables = ['Population', 'Income', 'Illiteracy',
+                               'Murder', 'HS.Grad', 'Frost', 'Area']
+        self.assertEqual(
+                    ['Population', 'Income', 'Murder', 'HS.Grad', 'Frost'], 
+                     R_Helper.variable_selection_using_backward_elimination(
+                                                       data_frame,
+                                                       prediction_variable,
+                                                       predictor_variables,
+                                                       p_to_remove=0.20
                                                     )
                      )
         
