1e7cea458b938f759a7805c32ebe93dea7f14a01
Krishna Kamath <krishna.kamath@gmail.com>
2012-09-20 17:30:48 -0500

    added new methodds to r_helper

--- library/r_helper.py
+++ library/r_helper.py
@@ -12,6 +12,13 @@ R = robjects.r
 
 class R_Helper(object):
     @staticmethod
+    def linear_regression_model(data_frame, prediction_variable, predictor_variables):
+        ''' Builds model using linear regression and returns it.
+        '''
+        predictor_string = '+'.join(predictor_variables)
+        formula_string = '%s ~ %s'%(prediction_variable, predictor_string)
+        return R.lm(formula_string, data=data_frame)
+    @staticmethod
     def variable_selection_using_backward_elimination(data_frame,
                                                       prediction_variable,
                                                       predictor_variables,
@@ -21,44 +28,24 @@ class R_Helper(object):
         Implements the backward elimination algorithm to select substet of 
         predictor variables using liniear regression. The elimnation is based
         on the algorithm described in Section 10.2 of "Practical Regression and
-        ANOVA using R" book by Julian J. Faraway
+        ANOVA using R" book by Julian J. Faraway.
         '''
-        def get_predictor_to_eliminate(data_frame,
-                                       prediction_variable,
-                                       predictor_variables):
-            predictor_string = '+'.join(predictor_variables)
-            formula_string = '%s ~ %s'%(prediction_variable, predictor_string)
-            g = R.lm(formula_string, data=data_frame)
+        def get_predictor_to_eliminate(data_frame, prediction_variable, predictor_variables):
+            g = R_Helper.linear_regression_model(data_frame, prediction_variable, predictor_variables,)
             summary = R.summary(g)
-            predictor_p_values = \
-                        list(summary.rx2('coefficients').rx(True, 'Pr(>|t|)'))
+            predictor_p_values = list(summary.rx2('coefficients').rx(True, 'Pr(>|t|)'))
             if len(predictor_p_values) > 1:
-                ltuo_pred_and_pred_p_value = zip(predictor_variables, 
-                                                 predictor_p_values[1:])
-                ltuo_pred_and_pred_p_value = filter(
-                                        lambda (_, p_val): p_val > p_to_remove,
-                                        ltuo_pred_and_pred_p_value
-                                    )
+                ltuo_pred_and_pred_p_value = zip(predictor_variables, predictor_p_values[1:])
+                ltuo_pred_and_pred_p_value = filter( lambda (_, p_val): p_val > p_to_remove, ltuo_pred_and_pred_p_value)
                 if len(ltuo_pred_and_pred_p_value) > 1:
-                    predictor_to_eliminate, _ = sorted(
-                                                    ltuo_pred_and_pred_p_value,
-                                                    key=itemgetter(1)
-                                                )[-1]
+                    predictor_to_eliminate, _ = sorted(ltuo_pred_and_pred_p_value, key=itemgetter(1))[-1]
                     return predictor_to_eliminate
         predictor_to_eliminate = True
         total_variables = len(predictor_variables)
         num_of_variables_eliminated = 0
         while predictor_variables and predictor_to_eliminate:
-            if debug:
-                print 'Eliminated %s variables of %s'%(
-                                                   num_of_variables_eliminated,
-                                                   total_variables
-                                                )
-            predictor_to_eliminate = get_predictor_to_eliminate(
-                                                        data_frame,
-                                                        prediction_variable,
-                                                        predictor_variables
-                                                    )
+            if debug: print 'Eliminated %s variables of %s'%(num_of_variables_eliminated, total_variables)
+            predictor_to_eliminate = get_predictor_to_eliminate(data_frame, prediction_variable, predictor_variables)
             if predictor_to_eliminate:
                 predictor_variables.remove(predictor_to_eliminate)
                 num_of_variables_eliminated+=1
@@ -82,4 +69,17 @@ class R_Helper(object):
             if type == 'int': data_for_df[column] = robjects.IntVector(data[column]['value'])
             elif type == 'float': data_for_df[column] = robjects.FloatVector(data[column]['value'])
         return robjects.DataFrame(data_for_df)
-            
+    @staticmethod
+    def get_parameter_values(model):
+        summary = R.summary(model)
+        parameter_names = list(summary.rx2('coefficients').names.rx2(1))
+        paramter_values = list(summary.rx2('coefficients').rx(True, 1))
+        return zip(parameter_names, paramter_values)
+    @staticmethod
+    def get_predicted_value(mf_parameter_names_to_values, mf_variable_name_to_value):
+        predicted_value = 0.0
+        mf_variable_name_to_value['(Intercept)'] = 1.0
+        for variable_name, value in mf_variable_name_to_value.iteritems():
+            predicted_value+=(mf_parameter_names_to_values.get(variable_name, 0.0)*value)
+        return predicted_value
+
--- library/tests/r_helper_tests.py
+++ library/tests/r_helper_tests.py
@@ -47,14 +47,38 @@ class R_HelperTests(unittest.TestCase):
         self.assertEqual(self.data_frame_json, R_Helper.get_json_for_data_frame(self.data_frame))
     def test_get_data_frame_from_json(self):
         returned_data_frame = R_Helper.get_data_frame_from_json(self.data_frame_json)
-        print returned_data_frame
         self.assertEqual(sorted(list(self.data_frame.colnames)), sorted(list(returned_data_frame.colnames)))
-        
-#    def test_null(self):
-#        current_path = os.path.realpath(__file__)
-#        data_frame = robjects.DataFrame.from_csvfile(
-#                current_path[:current_path.rindex('/')] + '/../data/state.df')
-#        print data_frame.colnames
+    def test_get_parameter_values(self):
+        current_path = os.path.realpath(__file__)
+        data_frame = robjects.DataFrame.from_csvfile(current_path[:current_path.rindex('/')] + '/../data/state.df')
+        prediction_variable = 'Life.Exp'
+        predictor_variables = ['Population', 'Income', 'Illiteracy', 'Murder', 'HS.Grad', 'Frost', 'Area']
+        expected_parameter_names_and_values = [
+                                               ('(Intercept)', 70.94322411112951),
+                                               ('Population', 5.1800363834827194e-05),
+                                               ('Income', -2.1804237825318798e-05),
+                                               ('Illiteracy', 0.033820321355290311),
+                                               ('Murder', -0.30112317045182957),
+                                               ('HS.Grad', 0.048929478881717045),
+                                               ('Frost', -0.0057350011035554654),
+                                               ('Area', -7.3831661447743889e-08)
+                                               ]
+
+        model = R_Helper.linear_regression_model(data_frame, prediction_variable, predictor_variables)
+        parameter_names_and_values = R_Helper.get_parameter_values(model)
+        self.assertEqual(expected_parameter_names_and_values, parameter_names_and_values)
+    def test_get_predicted_value(self):
+        current_path = os.path.realpath(__file__)
+        data_frame = robjects.DataFrame.from_csvfile(current_path[:current_path.rindex('/')] + '/../data/state.df')
+        prediction_variable = 'Life.Exp'
+        predictor_variables = ['Population', 'Income', 'Illiteracy', 'Murder', 'HS.Grad', 'Frost', 'Area']
+        model = R_Helper.linear_regression_model(data_frame, prediction_variable, predictor_variables)
+        mf_parameter_names_to_values = dict(R_Helper.get_parameter_values(model))
+        mf_variable_name_to_value = {'Population': 1.0}
+        self.assertEqual(
+                        '%0.5f'%70.9432759115, 
+                        '%0.5f'%R_Helper.get_predicted_value(mf_parameter_names_to_values, mf_variable_name_to_value)
+                     )
         
 if __name__ == '__main__':
     unittest.main()
