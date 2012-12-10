aaf04ace2cb02d499b3c04088d76bca93cec7eca
Krishna Kamath <krishna.kamath@gmail.com>
2012-12-10 11:33:42 -0600

    modified plotting

--- library/plotting.py
+++ library/plotting.py
@@ -146,11 +146,7 @@ class Map():
 
 class AnchoredText(AnchoredOffsetbox):
     def __init__(self, s, loc, pad=0.4, borderpad=0.5, prop=None, frameon=True):
-
-        self.txt = TextArea(s,
-                            minimumdescent=False)
-
-
+        self.txt = TextArea(s, minimumdescent=False)
         super(AnchoredText, self).__init__(loc, pad=pad, borderpad=borderpad,
                                            child=self.txt,
                                            prop=prop,
